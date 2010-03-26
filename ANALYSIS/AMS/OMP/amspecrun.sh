b5aee86b1087c849da2cde6b12076e6072d58084
Scott Michael <scamicha@indiana.edu>
2010-03-26 13:37:10 -0400

    Fixed run script.

--- ANALYSIS/AMS/OMP/amspecrun.sh
+++ ANALYSIS/AMS/OMP/amspecrun.sh
@@ -9,5 +9,5 @@ set skip=200
 set outfile=P.5am15AU.dat
 if (! -x amspec) then
     ifort -O3 -pad -align all -nocheck -shared-intel -openmp -r8 -mtune=core2 -mcmodel=medium -convert big_endian -o amspec amspec.f90
-fi
+endif
 ./amspec $jmax $kmax $lmax $start $finish $skip $outfile $jstart
