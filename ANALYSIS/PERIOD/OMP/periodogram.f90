bb775b8dc9057d2268c4025172159d7f1f6d4cbb
Scott Michael <scamicha@indiana.edu>
2010-08-12 14:37:53 -0400

    Finished Periodogram implementation ready for quarry testing.
    	modified:   ANALYSIS/PERIOD/OMP/periodogram.f90

--- ANALYSIS/PERIOD/OMP/periodogram.f90
+++ ANALYSIS/PERIOD/OMP/periodogram.f90
@@ -2,25 +2,26 @@
 PROGRAM PERIODOGRAM
   implicit none
   integer, parameter :: double = selected_real_kind(15,300)
+  integer, parameter :: OFAC = 4, HIFAC = 2
   integer :: jmax, kmax, lmax, jreq, istart, iend, iskip, numargs
   integer :: jmax2, kmax2, lmax2,modes,numfiles,jreq,fileiter
-  integer :: j,k,l,i,m,tstartind,tendind
+  integer :: j,k,l,i,m,tstartind,tendind,nout,nsub
   REAL(DOUBLE), PARAMETER :: torp = 1605.63
   REAL(DOUBLE), PARAMETER :: pi = 3.14159265358979323846d0
   REAL(DOUBLE), PARAMETER :: twopi = 2.d0*pi
   real(double) :: ROF3N,ZOF3N,DELT,TIME,ELOST,DEN,SOUND,OMMAX,tmassini
   real(double) :: dr,pi,torp,aujreq,tstart,tend
   real(double),dimension(:,:,:),allocatable :: rho,s,t,a,eps,omega
-  real(double),dimesion(:,:,:),allocatable :: angle,angsub
+  real(double),dimension(:,:,:),allocatable :: angle,angsub,results,frequencies
   real(double),dimension(:),allocatable :: omegavg,kappa,massum,timearr,am,bm,tsub
-  real(double),dimension(:,:),allocatable :: rhomid
+  real(double),dimension(:),allocatable :: X1,Y1,oneang
   character :: rhodir*80, savedir*80,rhofile*94,savedfile*94
   character :: jmaxin*8,kmaxin*8,lmaxin*8,istartin*8,iendin*8,iskipin*8,modein*8
   character :: filenum*8,aujreqin*10,tstartin*8,tendin*8
 
   numargs = IARGC()
 
-  if(numargs.ne. 11) then
+  if(numargs.ne. 13) then
      print*,"Incorrect number of arguments"
      STOP
   ENDIF
@@ -58,14 +59,13 @@ PROGRAM PERIODOGRAM
 
   allocate(timearr(numfiles))
   allocate(rho(jmax2,kmax2,lmax))
-  allocate(rhomid(jmax2,lmax))
   allocate(s(jmax2,kmax2,lmax))
   allocate(t(jmax2,kmax2,lmax))
   allocate(a(jmax2,kmax2,lmax))
   allocate(eps(jmax2,kmax2,lmax))
   allocate(omega(jmax2,kmax2,lmax))
-  allocate(omegavg(jmax2))
-  allocate(kappa(jmax2))
+  allocate(omegavg(jmax))
+  allocate(kappa(jmax))
   allocate(massum(jmax2))
   allocate(angle(jmax2,modes,numfiles))
 
@@ -103,11 +103,11 @@ PROGRAM PERIODOGRAM
 
 !$OMP DO DEFAULT(SHARED) REDUCTION(+:omegavg,massum)
   DO J=2,jmax1
-     omegavg(j) = 0.d0
+     omegavg(j-1) = 0.d0
      massum(j) = 0.d0
      DO k=1,kmax2
         DO l=1,lmax
-           omegavg(j) = omegavg(j) + omega(j,k,l)
+           omegavg(j-1) = omegavg(j-1) + omega(j,k,l)
            massum(j) = massum(j) + rho(j,k,l)
         ENDDO
      ENDDO
@@ -115,7 +115,7 @@ PROGRAM PERIODOGRAM
 !$OMP END DO
      
 !$OMP DO DEFAULT(SHARED)
-  DO J=2,jmax1
+  DO J=1,jmax
      omegavg(j) = omegavg(j)/massum(j)
      kappa(j) = sqrt(ABS(rhf(j)*omegavg(j)*(omegavg(j+1)- &
           omegavg(j-1))/dr + 4.d0*omegavg(j)**2))*(torp/(2.d0*pi))
@@ -135,15 +135,7 @@ PROGRAM PERIODOGRAM
      READ(8) RHO
      READ(8) time
      CLOSE(8)
-     timearr(I)=time
-!$OMP PARALLEL DO 
-     DO L=1,LMAX
-        DO J=1,JMAX2
-           rhomid(J,L) = rho(J,2,L)
-        ENDDO
-     ENDDO
-!OMP END PARALLEL DO
-
+     timearr(I)=time/torp
 
 !...   COMPUTE PHASE ANGLES
 !OMP PARALLEL DO DEFAULT(SHARED) &
@@ -157,8 +149,8 @@ PROGRAM PERIODOGRAM
         DO j = 2,jmax+1
            DO l=1,lmax
               phi = twopi*dble(l)/dble(lmax)
-              am(j) = am(j)+rhomid(j,l)*cos(dble(m)*phi)
-              bm(j) = bm(j)+rhomid(j,l)*sin(dble(m)*phi)
+              am(j) = am(j)+rho(j,2,l)*cos(dble(m)*phi)
+              bm(j) = bm(j)+rho(j,2,l)*sin(dble(m)*phi)
            ENDDO
            angle(J,M,I) = atan2(am(J),bm(J))+pi
         ENDDO
@@ -171,20 +163,21 @@ PROGRAM PERIODOGRAM
 !...    Extract time interval
 
   I = 1
-  DO WHILE (timearr(I).lt.(tstart*torp))
+  DO WHILE (timearr(I).lt.tstart)
      I = I+1
   ENDDO
   
   tstartind = I
 
-  DO WHILE (timearr(I).lt.(tend*torp))
+  DO WHILE (timearr(I).lt.tend)
      I = I+1
   ENDDO
 
   tendind = I
+  nsub = tendind - tstartind
   
-  allocate(tsub(tendind-tstartind))
-  allocate(angsub(jmax2,modes,tendind-tstartind))
+  allocate(tsub(nsub))
+  allocate(angsub(jmax2,modes,nsub))
 
   DO I = tstartind,tendind
      tsub(I+1-tstartind)=timearr(I)
@@ -195,8 +188,40 @@ PROGRAM PERIODOGRAM
      ENDDO
   ENDDO
 
+  nout = 0.5*OFAC*HIFAC*nsub
+  allocate(oneang(nsub))
+  allocate(X1(nout))
+  allocate(Y1(nout))
+  allocate(results(JMAX,modes,nout))
+  allocate(frequencies(JMAX,modes,nout))
+
+!OMP PARALLEL DO DEFAULT(SHARED) &
+!OMP PRIVATE(oneang,J,I,X1,Y1,prob)
   DO M=1,modes
      DO J=2,JMAX+1
+        DO I = 1,nsub
+           oneang(I) = angsub(J,M,I)
+        ENDDO
+        
+        call FASPER(tsub,oneang,nsub,OFAC,HIFAC,X1,Y1,nout,prob)
+        
+        DO I = 1,nout
+           results(J,M,I) = Y1(I)
+           frequencies(J,M,I) = X1(I)
+        ENDDO
+     ENDDO
+  ENDDO
+!OMP END PARALLEL DO
+
+  OPEN(UNIT=12,FILE=TRIM(outfile),FORM='UNFORMATTED')
+  WRITE(12) JMAX,modes,nout
+  WRITE(12) results
+  WRITE(12) frequencies
+  WRITE(12) omegavg
+  WRITE(12) kappa
+  CLOSE(12)
+
+           
         
 
   
@@ -207,125 +232,217 @@ PROGRAM PERIODOGRAM
 
 END PROGRAM PERIODOGRAM
 
-SUBROUTINE PERIOD(
+SUBROUTINE FASPER(X,Y,N,OFAC,HIFAC,WK1R,WK2R,NOUT,PROB)
+  IMPLICIT NONE
 
-PRO slowperiod
+  INTEGER, PARAMETER :: MACC=4
+  INTEGER :: NOUT,NFREQT,NFREQ,NDIM,FNDIM,J,K
+  REAL(DOUBLE) :: X(*),Y(*),WK1R(*),WK2R(*)
+  REAL(DOUBLE),DIMENSION(:),allocatable :: wk1,wk2
+  REAL(DOUBLE) :: AVE,VAR,XMIN,XMAX,FAC,CK,CKK,DF,PMAX
+  REAL(DOUBLE) :: HYPO,HC2WT,HS2WT,CWT,SWT,DEN,CTERM
+  REAL(DOUBLE) :: STERM,EXPY,EFFM,PROB
 
-TWOPI = 6.283185307179586476d0
+  NFREQT = OFAC*HIFAC*N*MACC
+  NFREQ = 64
+  DO WHILE (NFREQ.lt.NFREQT)
+     NFREQ = NFREQ*2
+  ENDDO
+  NDIM = NFREQ*2
+  allocate(wk1(NDIM))
+  allocate(wk2(NDIM))
+  CALL AVEVAR(Y,N,AVE,VAR)
+  XMIN = MIN(X)
+  XMAX = MAX(X)
+  XDIF = XMAX - XMIN
+  wk1(:) = 0.d0
+  wk2(:) = 0.d0
+  FAC = NDIM/(XDIF*OFAC)
+  FNDIM = NDIM
+  DO J=1,N
+     CK = 1.d0+DMOD((X(J)-XMIN)*FAC,FNDIM)
+     CKK = 1.d0+DMOD(2.d0*(CK-1.d0),FNDIM)
+     CALL SPREAD(Y(J)-AVE,wk1,NDIM,CK,MACC)
+     CALL SPREAD(1.d0,wk2,NDIM,CKK,MACC)
+  ENDDO
+  CALL REALFT(wk1,NFREQ)
+  CALL REALFT(wk2,NFREQ)
+  DF = 1.d0/(XDIF*OFAC)
+  K = 3
+  PMAX = -1.d0
 
+  DO J=1,NOUT
+     HYPO = sqrt(wk2(K)**2+wk2(k+1)**2)
+     HC2WT = 0.5d0*wk2(k)/HYPO
+     HS2WT = 0.5d0*wk2(k+1)/HYPO
+     CWT = sqrt(0.5d0+HC2WT)
+     SWT = sign(sqrt(0.5d0-HC2WT),HS2WT)
+     DEN = 0.5d0*N+HC2WT*wk2(k)+HS2WT*wk2(k+1)
+     CTERM = (CWT*wk1(k) + SWT*wk1(k+1))**2/DEN
+     STERM = (CWT*wk1(K+1)-SWT*wk1(k))**2/(N-DEN)
+     wk1(J) = J*DF
+     wk2(J) = (CTERM+STERM)/(2.d0*VAR)
+     IF (wk2(J).gt.PMAX) PMAX = wk2(J)
+     K = K+2
+  ENDDO
+  EXPY = exp(-PMAX)
+  EFFM = 2.d0*NOUT/OFAC
+  PROB = EFFM*EXPY
+  IF (PROB.gt.0.01d0)PROB = 1.d0-(1.d0-EXPY)**EFFM
+  DO J = 1,NOUT
+     WK1R(J) = wk1(J)
+     WK2R(J) = wk2(J)
+  ENDDO
+  RETURN
+END SUBROUTINE FASPER
+  
 
-MAXSIZE = 500000L
+  
 
-mintime    = 12.03d0
-maxtime    = 19.57d0
-str =''
-timestring = ''
-stepdum   = 0L
-starcount = 0L
-percount  = 0L
-macc      = 4L
-ofac      = 4L
-hifac     = 0.5d0
-xdum      = 0.d0
-ydum      = 0.d0
-rdum      = 0.d0
-timedum   = 0.d0
-angdum    = 0.d0
-dz        = 0.202180656d0
-mratio    = 8.075411422d0
-pi        = 3.1415926353892d0
-maxx      = 0.d0
-minx      = 0.d0
-maxy      = 0.d0
-miny      = 0.d0
-maxr      = 0.d0
-
-stepcom = LONARR(MAXSIZE)
-xcom    = DBLARR(MAXSIZE)
-ycom    = DBLARR(MAXSIZE)
-rcom    = DBLARR(MAXSIZE)
-timecom = DBLARR(MAXSIZE)
-angcom  = DBLARR(MAXSIZE)
-
-tconv   = 1605.63d0
-
-openr,lun1,"comfile50.dat",/GET_LUN
-
- WHILE NOT EOF(lun1) DO BEGIN
-     READF,lun1,str,FORMAT='(A256)'
-     READS,str,stepdum,timedum,xdum,ydum,rdum,angdum
-     stepcom(starcount)=stepdum
-     rcom(starcount) = rdum
-     xcom(starcount) = xdum 
-     ycom(starcount) = ydum
-     timecom(starcount)=timedum
-     angcom(starcount)=angdum
-     starcount++
- ENDWHILE
-
- rcom    = rcom(0:starcount-1)
- xcom    = xcom(0:starcount-1)
- ycom    = ycom(0:starcount-1)
- stepcom = stepcom(0:starcount-1)
- timecom = timecom(0:starcount-1)
- angcom  = angcom(0:starcount-1)
- x       = timecom/tconv
- y       = ycom
- indexsub= WHERE((x gt mintime) and (x lt maxtime))
- x       = x(indexsub)
- y       = y(indexsub)
-n       = starcount 
- wi      = DBLARR(n)
- wpi     = DBLARR(n)
- wpr     = DBLARR(n)
- wr      = DBLARR(n)
- nout    = LONG(0.5*ofac*hifac*n) 
- px      = DBLARR(nout)
- py      = DBLARR(nout) 
- ave     = MEAN(y,/DOUBLE)
- var     = VARIANCE(y,/DOUBLE)
- IF(var eq 0.d0) THEN BEGIN
-     print,"Error: zero variance in period"
-     RETURN
- ENDIF
-
- xmax   = MAX(x,MIN=xmin)
- xdif   = xmax-xmin
- xave   = 0.5d0*(xmax+xmin)
- pymax  = 0.d0
- jmax   = 0L
- pnow   = 1.d0/(xdif*ofac)
- arg    = TWOPI*((x-xave)*pnow)
- wpr    = -2.d0*(sin(0.5d0*arg))^2.d0
- wpi    = sin(arg)
- wr     = cos(arg)
- wi     = wpi
- FOR i=0L,nout-1L DO BEGIN
-     px(i)  = pnow
-     sumsh  = TOTAL(wr*wi,/DOUBLE)
-     sumc   = TOTAL((wr-wi)*(wr+wi),/DOUBLE)
-     wtau   = 0.5d0*ATAN(2.d0*sumsh,sumc)
-     swtau  = SIN(wtau)
-     cwtau  = COS(wtau)
-     sums   = TOTAL(((wi*cwtau)-(wr*swtau))^2.d0,/DOUBLE)
-     sumc   = TOTAL(((wr*cwtau)+(wi*swtau))^2.d0,/DOUBLE)
-     yy     = y - ave
-     sumsy  = TOTAL(((wi*cwtau)-(wr*swtau))*yy,/DOUBLE)
-     sumcy  = TOTAL(((wr*cwtau)+(wi*swtau))*yy,/DOUBLE)
-     wtemp  = wr
-     wr     = ((wr*wpr)-(wi*wpi))+wr
-     wi     = ((wi*wpr)+(wtemp*wpi))+wi
-     py(i)  = 0.5d0*(sumcy*sumcy/sumc+sumsy*sumsy/sums)/var
-     IF (py(i) ge pymax) THEN BEGIN
-         pymax = py(i)
-         jmax  = i 
+!...    Following routines from Numerical Recipes.
+SUBROUTINE FOUR1(data, nn)
+  INTEGER :: n,mmax,m,j,istep,i,nn
+  REAL(DOUBLE) :: wtemp,wr,wpr,wpi,wi,theta,tempr,tempi
+  REAL(DOUBLE) :: data(*)
+  
+  n = ISHFT(nn,1)
+  j = 1
+  DO I=1,N-1,2
+     IF(j.gt.i) THEN
+        call SWAP(data(j),data(i))
+        call SWAP(data(j+1),data(i+1))
      ENDIF
-     pnow += 1.d0/(ofac*xdif)
- ENDFOR
+     m = ISHFT(n,-1)
+     DO WHILE ((m.ge.2).and.(j.gt.m))
+        j = j - m
+        m = ISHFT(m,-1)
+     ENDDO
+     j = j + m
+  ENDDO
 
- expy  = exp(-pymax)
- effm  = 2.d0*nout/ofac
- prob  = effm*expy
- IF (prob gt 0.01d0) THEN prob = 1.d0 - (1.d0-expy)^effm
+  mmax = 2
+  DO WHILE (n.gt.mmax)
+     istep = ISHFT(mmax,1)
+     theta = (6.28318530717959/mmax)
+     wtemp = sin(0.5d0*theta)
+     wpr = -2.d0*wtemp**2
+     wpi = sin(theta)
+     wr = 1.d0
+     wi = 0.d0
+     DO M=1,MMAX-1,2
+        DO I=M,N,istep
+           j = i + mmax
+           tempr = wr*data(j)-wi*data(j+1
+           tempi = wr*data(j+1)+wi*data(j)
+           data(j) = data(i)-tempr
+           data(j+1) = data(i+1)-tempi
+           data(i) = data(i)+tempr
+           data(i+1) = data(i+1)+tempi
+        ENDDO
+        wtemp = wr
+        wr = wtemp*wpr-wi*wpi+wr
+        wi = wi*wpr+wtemp*wpi+wi
+     ENDDO
+     mmax=istep
+  ENDDO
+  RETURN
+END SUBROUTINE FOUR1
 
- print, prob
+SUBROUTINE REALFT(dataft,n)
+
+  INTEGER :: i, i1,i2,i3,i4,np3,dum
+  REAL :: c1=0.5,c2,h1r,h1i,h2r,h2i
+  REAL(DOUBLE) :: wr,wi,wpr,wpi,wtemp,theta
+  REAL(DOUBLE) :: dataft(*)
+
+  dum = ISHFT(n,-1)
+  theta = 3.141592653589793/dble(dum)
+  c2 = 0.5
+  call four1(dataft,dum)
+  wtemp = sin(0.5d0*theta)
+  wpr = -2.d0*wtemp**2
+  wpi = sin(theta)
+  wr = 1.d0+wpr
+  wi = wpi
+  np3 = n+3
+  dum = ISHFT(n,-2)
+  DO i=2,dum
+     i1 = i+i+1
+     i2 = 1+i1
+     i3 = np3-i2
+     i4 = 1+i3
+     h1r = c1*(dataft(i1)+dataft(i3))
+     h1i = c1*(dataft(i2)-dataft(i4))
+     h2r = -c2*(dataft(i2)+dataft(i4))
+     h2i = c2*(dataft(i1)-dataft(i3))
+     data(i1) = h1r+wr*h2r-wi*h2i
+     data(i2) = h1i+wr*h2i+wi*h2r
+     data(i3) = h1r-wr*h2r+wi*h2i
+     data(i1) = -h1i+wr*h2i+wi*h2r
+     wtemp = wr
+     wr = wtemp*wpr-wi*wpi+wr
+     wi = wi*wpr+wtemp*wpi+wi
+  ENDDO
+  h1r = dataft(1)
+  dataft(1) = h1r + dataft(2)
+  dataft(2) = h1r - dataft(2)
+  RETURN
+END SUBROUTINE REALFT
+  
+SUBROUTINE SPREAD(Y,YY,N,X,M)
+  IMPLICIT NONE
+  REAL(DOUBLE) :: Y,X,fac
+  REAL(DOUBLE) :: Y(*)
+  INTEGER :: n,m,ix,ilo,ihi,nden,j
+  INTEGER,PARAMETER :: nfac(10) /1,1,2,6,24,120,720,5040,40320,362880/
+  
+  ix = int(x)
+  if (x.eq.dble(ix)) THEN
+     yy(ix) = yy(ix) + y
+  ELSE
+     ilo = MIN(MAX(int(x-0.5*m+1.0),1),n-m+1)
+     ihi = ilo+m-1
+     nden = nfac(m)
+     fac = x-ilo
+     DO J=ilo+1,IHI
+        fac = fac*(x-j)
+     ENDDO
+     yy(ihi) = yy(ihi)+y*fac/(nden*(x-ihi))
+     DO J=ihi-1,ilo,-1
+        nden=(nden/(j+1-ilo))*(j-ihi)
+        yy(j) = yy(j) + y*fac/(nden*(x-j))
+     ENDDO
+  ENDIF
+  RETURN
+END SUBROUTINE SPREAD
+     
+SUBROUTINE SWAP(A,B)
+  REAL(DOUBLE) :: A,B,TMP
+  TMP = A
+  a = B
+  B = TMP
+  RETURN
+END SUBROUTINE SWAP
+
+SUBROUTINE AVEVAR(X,NUM,AVG,VAR)
+  IMPLICIT NONE
+  
+  INTEGER :: NUM, J
+  REAL(DOUBLE) :: X(*)
+  REAL(DOUBLE) :: AVG,VAR,S,EP
+
+  AVG = 0.d0
+  DO J=1,NUM
+     AVG = AVG + X(J)
+  ENDDO
+  AVG = AVG/NUM
+  VAR = 0.d0
+  EP  = 0.d0
+  DO J=1,N
+     S = X(J)-AVG
+     EP = EP + S
+     VAR = VAR + S**2
+  ENDDO
+  VAR = (VAR-EP**2/NUM)/(NUM-1)
+  RETURN
+END SUBROUTINE AVEVAR
