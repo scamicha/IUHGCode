e6b4ae5fa50a43e9870fbccb79f9a58ab38167f9
Scott Michael <scamicha@indiana.edu>
2010-10-06 14:15:05 -0400

    Tested Q plot with EOS, will compile and run now.
    	modified:   ONEOFF/Q_EOS/Makefile
    	modified:   ONEOFF/Q_EOS/globals.h
    	modified:   ONEOFF/Q_EOS/hydroparam.h
    	deleted:    ONEOFF/Q_EOS/initengtable.f
    	new file:   ONEOFF/Q_EOS/initengtable.f90
    	modified:   ONEOFF/Q_EOS/qplot_eos.f90
    
    Untracked files:
      (use "git add <file>..." to include in what will be committed)
    
    	ONEOFF/Q_EOS/qeos

--- ONEOFF/Q_EOS/globals.h
+++ ONEOFF/Q_EOS/globals.h
@@ -1,20 +1,6 @@
-!-*-fortran-*-
-C***********************************************************************
-C     
-      real(KIND=8), dimension(JMAX2)    :: sigma_fc,
-     &        cs_fc, omega_fc, tgrav, treyn,gt1,
-     &        mass_fc,kepler_fc,rtv,oav,vrav, cs_sig
-
-      real(KIND=8) :: vphiav,econv,rdisk,kconst
-
-      real(KIND=8), dimension(JMAX2) :: masss,masse,mdot
-
-      COMMON /alphadisk/sigma_fc,gt1,
-     &        cs_fc, omega_fc, tgrav, treyn,
-     &        mass_fc,kepler_fc,rtv,oav,vrav,
-     &        cs_sig, masss,masse,mdot,
-     &        vphiav,econv,rdisk,kconst
- 
+!-*-f90-*-
+!***********************************************************************
+!
 
       real*8 rcloud,constp,delt,bdytem,den,time,cormas,epscen
       COMMON /BLOK7/RCLOUD,CONSTP,DELT,BDYTEM,DEN,TIME,CORMAS,epscen 
@@ -25,53 +11,37 @@ C
       integer jin,itype
       real*8 tmassini,tmassadd,tmassout,tmassacc
       real*8 starphi,diskphi,tinphi
-      COMMON /GAP/starphi(jmax2,kmax2,lmax),
-     &     tinphi(jmax2,kmax2,lmax),
-     &     diskphi(jmax2,kmax2,lmax),
-     &     tmassini,tmassadd,tmassout,tmassacc,
-     &     jin,itype
+      COMMON /GAP/starphi(jmax2,kmax2,lmax),                     &             
+           tinphi(jmax2,kmax2,lmax),                             &
+           diskphi(jmax2,kmax2,lmax),                            &
+           tmassini,tmassadd,tmassout,tmassacc,                  &
+           jin,itype
 
 
-      REAL*8 JN,s,t,a,u,w,omega
-      COMMON /EOM/
-     &     S(JMAX2,KMAX2,LMAX),
-     &     T(JMAX2,KMAX2,LMAX),
-     &     A(JMAX2,KMAX2,LMAX),
-     &     U(JMAX2,KMAX2,LMAX),
-     &     W(JMAX2,KMAX2,LMAX),
-     &     JN(JMAX2,KMAX2,LMAX),
-     &     OMEGA(JMAX2,KMAX2,LMAX)
+      REAL*8 s,t,a
+      COMMON /EOM/S(JMAX2,KMAX2,LMAX),T(JMAX2,KMAX2,LMAX),       &
+           A(JMAX2,KMAX2,LMAX)
 
       real*8 p,cv,eps
       real*8 phi,rho,tempc
-      COMMON /STATES/
-     &     P(JMAX2,KMAX2,LMAX),
-     &     CV(JMAX2,KMAX2,LMAX),
-     &     EPS(JMAX2,KMAX2,LMAX),
-     &     TEMPC(JMAX2,KMAX2,LMAX)
+      COMMON /STATES/P(JMAX2,KMAX2,LMAX),CV(JMAX2,KMAX2,LMAX),   &
+           EPS(JMAX2,KMAX2,LMAX),TEMPC(JMAX2,KMAX2,LMAX)
 
 
-      COMMON /POIS/
-     &     PHI(POT3JMAX2,POT3KMAX2,LMAX),
-     &     RHO(JMAX2,KMAX2,LMAX),
+      COMMON /POIS/PHI(POT3JMAX2,POT3KMAX2,LMAX),RHO(JMAX2,KMAX2,LMAX)
 
-      real*8 Msyscgs,PKcgs,Tconv,Sconv,Dconv,Pconv,sigma,
-     &     rhoconv,engconv,bkmpcode
-      COMMON /CONVERT/
-     &     Msyscgs,PKcgs,Tconv,Sconv,
-     &     Dconv,Pconv,sigma,rhoconv,engconv,bkmpcode
+      real*8 Msyscgs,PKcgs,Tconv,Sconv,Dconv,Pconv,sigma,        &
+           rhoconv,engconv,bkmpcode
+      COMMON /CONVERT/Msyscgs,PKcgs,Tconv,Sconv,                 &
+           Dconv,Pconv,sigma,rhoconv,engconv,bkmpcode
 
       real*8 r,z,rhf,zhf,rof3n,zof3n
-      COMMON /GRID/jreq,
-     &     R(pot3jmax2),
-     &     Z(POT3KMAX2),
-     &     RHF(POT3JMAX2),
-     &     ZHF(POT3KMAX2),
-     &     ROF3N,ZOF3N
+      COMMON /GRID/jreq,R(pot3jmax2),Z(POT3KMAX2),               &
+           RHF(POT3JMAX2),ZHF(POT3KMAX2),ROF3N,ZOF3N
 
       real*8 :: temptable,engtable,gammatable,muc
-      common /engtables/temptable(TTABLE),engtable(TTABLE),
-     &        gammatable(TTABLE),muc
+      common /engtables/temptable(TTABLE),engtable(TTABLE),      &
+              gammatable(TTABLE),muc
 
       real*8 etotfl, efl, eflufftot, gamma1
       COMMON /etally/ etotfl, efl, eflufftot,gamma1(JMAX2,KMAX2,LMAX)
@@ -79,7 +49,7 @@ C
       real*8 tempk
       COMMON /COOLING/tempk(JMAX2,KMAX2,LMAX)
 
-C$OMP THREADPRIVATE(/GRID/)
+!$OMP THREADPRIVATE(/GRID/)
       
 
 
--- ONEOFF/Q_EOS/qplot_eos.f90
+++ ONEOFF/Q_EOS/qplot_eos.f90
@@ -1,29 +1,31 @@
 PROGRAM  QPLOT_EOS
 
   IMPLICIT NONE
-      
+
   INCLUDE 'hydroparam.h'
   INCLUDE 'globals.h'
-  
+
   integer, parameter :: double = selected_real_kind(15,300)
   REAL(DOUBLE), PARAMETER :: torp = 1605.63
   REAL(DOUBLE), PARAMETER :: pi = 3.14159265358979323846d0
   REAL(DOUBLE), PARAMETER :: twopi = 2.d0*pi
-  INTEGER I,NUMFILES,COUNTER
-  REAL(DOUBLE) :: time1,time2,dr
+  INTEGER I,NUMFILES,COUNTER,J,K,L,JREQ
+  REAL(DOUBLE) :: dr,dz,elost,sound,ommax
   REAL(DOUBLE), DIMENSION(:,:),ALLOCATABLE :: qomega,qkappa
-  REAL(DOUBLE), DIMENSION(:),ALLOCATABLE :: timearr
-  CHARACTER :: savedfile*80,filenum*8
+  REAL(DOUBLE), DIMENSION(:),ALLOCATABLE :: timearr,omegavg,csavg,sigmavg
+  REAL(DOUBLE), DIMENSION(:),ALLOCATABLE :: kappa
+  CHARACTER :: savedfile*80,filenum*8,qfile*80
   LOGICAL FILE_EXIST
-
-  time1 = 13.0
-  time2 = 21.0
 
   NUMFILES = ((IEND - ISTART)/ISKIP) + 1
   
   ALLOCATE(qomega(JMAX2,NUMFILES))
   ALLOCATE(qkappa(JMAX2,NUMFILES))
   ALLOCATE(timearr(NUMFILES))
+  ALLOCATE(omegavg(JMAX2))
+  ALLOCATE(csavg(JMAX2))
+  ALLOCATE(sigmavg(JMAX2))
+  ALLOCATE(kappa(JMAX2))
 
   CALL INIT()
   CALL INITENGTABLE()
@@ -35,7 +37,7 @@ PROGRAM  QPLOT_EOS
      savedfile=trim(datadir)//'saved.'//filenum
      INQUIRE(file=savedfile,exist=FILE_EXIST)
      IF(FILE_EXIST) THEN
-        OPEN(UNIT=8, FILE=trim(savedfile),FORM='UNFORMATTED',  $
+        OPEN(UNIT=8, FILE=trim(savedfile),FORM='UNFORMATTED',  &
         STATUS="OLD")
       
         READ(8) S
@@ -59,7 +61,7 @@ PROGRAM  QPLOT_EOS
 
      IF (counter == 0) then
         DO j = 1,jmax2 
-           r(j) = (FLOAT(j-1)*dr+0.5*dr)*rdiskau/(FLOAT(JREQ-1)*dr  $
+           r(j) = (FLOAT(j-1)*dr+0.5*dr)*rdiskau/(FLOAT(JREQ-1)*dr  &
            +0.5*dr)
            rhf(J) = r(J)*(FLOAT(JREQ-1)*dr+0.5*dr)/rdiskau
         ENDDO
@@ -74,7 +76,7 @@ PROGRAM  QPLOT_EOS
 !$OMP DO SCHEDULE(STATIC)
          DO J=1,JMAX2
             DO L=1,LMAX
-                  p(J,2,L) = bkmpcgs*tempk(J,2,L)*rhoconv*rho(J,2,L)   $
+                  p(J,2,L) = bkmpcgs*tempk(J,2,L)*rhoconv*rho(J,2,L)   &
                   / (muc*pconv)
             ENDDO
          ENDDO
@@ -92,7 +94,7 @@ PROGRAM  QPLOT_EOS
            omegavg(J) = omegavg(J)+ a(j,k,l)/rho(j,k,l)/rhf(j)**2
            csavg(J) = csavg(J) + sqrt(gamma1(J,2,L)*p(J,2,L)/rho(J,2,L))
         ENDDO
-        sigmaavg(J) = 2.d0*sigmavg(J)/DBLE(LMAX)
+        sigmavg(J) = 2.d0*sigmavg(J)/DBLE(LMAX)
         omegavg(J)  = omegavg(J)/DBLE(LMAX)
         csavg(J)    = csavg(J)/DBLE(LMAX)
      ENDDO
@@ -100,7 +102,7 @@ PROGRAM  QPLOT_EOS
 
 !$OMP DO SCHEDULE(STATIC)
      DO J=3,JMAX-1
-        kappa(j)=sqrt(ABS(rhf(j)*omegavg(j)*(omegavg(j+1)-   $
+        kappa(j)=sqrt(ABS(rhf(j)*omegavg(j)*(omegavg(j+1)-   &
         omegavg(j-1))/dr + 4.d0*omegavg(j)**2))
         qkappa(j,counter)=kappa(j)*csavg(j)/(pi*sigmavg(j))
         qomega(j,counter)=omegavg(j)*csavg(j)/(pi*sigmavg(j))
@@ -109,15 +111,21 @@ PROGRAM  QPLOT_EOS
 
 !$OMP END PARALLEL
 
-     OPEN(UNIT=15,FILE=qfile,FORM='UNFORMATTED')
-     WRITE(15)JMAX2,NUMFILES,COUNTER
-     WRITE(15)ISTART,IEND,ISKIP
-     WRITE(15)timearr
-     WRITE(15)r
-     WRITE(15)qomega
-     WRITE(15)qkappa
-     CLOSE(15)
+  ENDDO
 
-     DEALLOCATE(timearr)
-     DEALLOCATE(qomega)
-     DEALLOCATE(qkappa)
+  write (filenum,'(I8.8)')IEND
+  qfile=trim(outdir)//trim(outfile)//filenum
+
+  OPEN(UNIT=15,FILE=qfile,FORM='UNFORMATTED')
+  WRITE(15)JMAX2,NUMFILES,COUNTER
+  WRITE(15)ISTART,IEND,ISKIP
+  WRITE(15)timearr
+  WRITE(15)r
+  WRITE(15)qomega
+  WRITE(15)qkappa
+  CLOSE(15)
+
+  DEALLOCATE(timearr)
+  DEALLOCATE(qomega)
+  DEALLOCATE(qkappa)
+END PROGRAM QPLOT_EOS
