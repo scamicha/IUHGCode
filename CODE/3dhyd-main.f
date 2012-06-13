52474b70771cd96df4be43e88328ae2e13acac9b
Scott Michael <smichael@tg-login1.blacklight.psc.teragrid.org>
2012-06-13 14:38:10 -0400

    Fixed parentheses bug

--- CODE/3dhyd-main.f
+++ CODE/3dhyd-main.f
@@ -1017,7 +1017,7 @@ C...Diagnostic Information for COM and M=1 power...
             if (jmin.gt.2) write(8) tmassini,tmass,tmassadd,tmassout
      &        ,tmassacc,totcool,totdflux,totheat,totirr,etotfl,eflufftot
             if (planet) write(8) planetx,planety,planetvx,planetvy
-            close(8
+            close(8)
 
 !$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(k,j,l)
             do l=1,lmax
